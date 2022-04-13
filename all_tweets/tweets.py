1b719bc75d59be3352fbb249a5bdf5a2154555c0
Jack Madison <madison@sfu.ca>
2022-04-13 15:14:18 -0700

    may 1 to 7

--- all_tweets/tweets.py
+++ all_tweets/tweets.py
@@ -7,17 +7,17 @@ import requests
 # If this code has been downloaded from github, please create a new Python file called
 # twitter_authentication containing bearer_token = "INSERT YOUR BEARER TOKEN HERE" in the
 # same directory as this Python file 
-from twitter_authentication import bearer_token
+from twitter_authentication import bearer_token_1
 
-client = tweepy.Client(bearer_token, wait_on_rate_limit=True)
+client = tweepy.Client(bearer_token_1, wait_on_rate_limit=True)
 
 symptoms_tweets = []
 
 for response in tweepy.Paginator(client.search_all_tweets, 
                                 query = '-is:retweet -is:nullcast has:geo place_country:JP',
                                 tweet_fields = ['author_id', 'created_at', 'geo', 'id', 'lang', 'public_metrics', 'source', 'text'],
-                                start_time = '2020-05-01T00:00:00+09:00',
-                                end_time = '2020-05-07T23:59:59+09:00',
+                                start_time = '2020-05-08T00:00:00+09:00',
+                                end_time = '2020-05-14T23:59:59+09:00',
                                 max_results=500):
     time.sleep(1)
     symptoms_tweets.append(response)
