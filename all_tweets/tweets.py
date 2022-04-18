8e072ec31d78c3ab6ce4a47530b526fc73a5c4ec
Jack Madison <madison@sfu.ca>
2022-04-18 14:54:12 -0700

    Update tweets.py

--- all_tweets/tweets.py
+++ all_tweets/tweets.py
@@ -5,17 +5,17 @@ import tweepy
 # If this code has been downloaded from github, please create a new Python file called
 # twitter_authentication containing bearer_token = "INSERT YOUR BEARER TOKEN HERE" in the
 # same directory as this Python file 
-from twitter_authentication import bearer_token_1
+from twitter_authentication import bearer_token_3
 
-client = tweepy.Client(bearer_token_1, wait_on_rate_limit=True)
+client = tweepy.Client(bearer_token_3, wait_on_rate_limit=True)
 
 symptoms_tweets = []
 
 for response in tweepy.Paginator(client.search_all_tweets, 
                                 query = '-is:retweet -is:nullcast has:geo place_country:JP',
                                 tweet_fields = ['author_id', 'created_at', 'geo', 'id', 'lang', 'public_metrics', 'source', 'text'],
-                                start_time = '2019-02-08T00:00:00+09:00',
-                                end_time = '2019-02-14T23:59:59+09:00',
+                                start_time = '2019-04-01T00:00:00+09:00',
+                                end_time = '2019-04-07T23:59:59+09:00',
                                 max_results=500):
     time.sleep(1)
     symptoms_tweets.append(response)
@@ -38,4 +38,4 @@ for response in symptoms_tweets:
 # Change this list of dictionaries into a dataframe
 tweets = pd.DataFrame(result)
 
-tweets.to_csv('./all_tweets/tweet_data/2019/feb8_14.csv', index = False)
+tweets.to_csv('./all_tweets/tweet_data/2019/apr1_7.csv', index = False)
