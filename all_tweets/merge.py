6c79ffa76e9e90722fddd71cf407c0465219ac06
Jack Madison <madison@sfu.ca>
2022-04-22 14:54:07 -0700

    merge months

--- all_tweets/merge.py
+++ all_tweets/merge.py
@@ -1,22 +1,21 @@
 # The following python code merges the tweet data. I decided to pull the data in smaller
 # batches to aviod going over the rate limit and losing the entire dataframe.
-
 import pandas as pd
 import numpy as np
 
-may1_7 = pd.read_csv('./all_tweets/tweet_data/2020/may1_7.csv')
-may8_14 = pd.read_csv('./all_tweets/tweet_data/2020/may8_14.csv')
-may15_21 = pd.read_csv('./all_tweets/tweet_data/2020/may15_21.csv')
-may22_31 = pd.read_csv('./all_tweets/tweet_data/2020/may22_31.csv')
+week1 = pd.read_csv('./all_tweets/tweet_data/2019/feb1_7.csv')
+week2 = pd.read_csv('./all_tweets/tweet_data/2019/feb8_14.csv')
+week3 = pd.read_csv('./all_tweets/tweet_data/2019/feb15_21.csv')
+week4 = pd.read_csv('./all_tweets/tweet_data/2019/feb22_28.csv')
 
-may = may1_7.append(may8_14)
-may = may.append(may15_21)
-may = may.append(may22_31)
+month = week1.append(week2)
+month = month.append(week3)
+month = month.append(week4)
 
-may = may.drop_duplicates()
+month = month.drop_duplicates()
 
-may = may.sort_values(by = 'tweet_created_at')
+month = month.sort_values(by = 'tweet_created_at')
 
-may = may.reset_index(drop = True)
+month = month.reset_index(drop = True)
 
-may.to_csv('./all_tweets/tweet_data/2020/05_2020.csv')
+month.to_csv('./all_tweets/tweet_data/2019/02_2019.csv')
