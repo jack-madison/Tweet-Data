6261db9abed28dd49eb7e1fcf319b6e3b2174a20
Jack Madison <madison@sfu.ca>
2022-06-28 17:53:44 -0700

    update

--- locations/tweet_locations.py
+++ locations/tweet_locations.py
@@ -13,7 +13,7 @@ from twitter_authentication import bearer_token_7
 from twitter_authentication import bearer_token_8
 
 # Read in the CSV of tweets
-tweets = pd.read_csv('https://media.githubusercontent.com/media/jack-madison/Twitter-Sentiment-Analysis/main/1_tweets_raw/2018/03_2018.csv')
+tweets = pd.read_csv('https://media.githubusercontent.com/media/jack-madison/Twitter-Sentiment-Analysis/main/1_tweets_raw/2018/04_2018.csv')
 
 # Read in the dataframe of locations already collected
 locations = pd.read_csv('./locations/locations.csv')
